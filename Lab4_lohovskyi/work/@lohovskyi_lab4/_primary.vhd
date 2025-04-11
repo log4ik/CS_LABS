755fa9d2629ab9b41e1ff9013cad34b98e519910
de_coder <113625215+log4ik@users.noreply.github.com>
2025-04-11 13:05:31 +0300

    Add files via upload

---
+++ Lab4_lohovskyi/work/@lohovskyi_lab4/_primary.vhd
@@ -0,0 +1,11 @@
+library verilog;
+use verilog.vl_types.all;
+entity Lohovskyi_lab4 is
+    port(
+        x1              : in     vl_logic;
+        x2              : in     vl_logic;
+        x3              : in     vl_logic;
+        f1              : out    vl_logic;
+        f2              : out    vl_logic
+    );
+end Lohovskyi_lab4;
